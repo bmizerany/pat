4b362ec055e1612aee7795b7b38cdf08b3e05ca3
Blake Mizerany <blake.mizerany@gmail.com>
2012-01-27 11:28:15 -0800

    fix benchmark - thank you Evan Shaw

--- bench_test.go
+++ bench_test.go
@@ -8,11 +8,13 @@ import (
 func BenchmarkPatternMatching(b *testing.B) {
 	p := New()
 	p.Get("/hello/:name", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){}))
-	r, err := http.NewRequest("GET", "/hello/blake", nil)
-	if err != nil {
-		panic(err)
-	}
 	for n := 0; n < b.N; n++ {
+		b.StopTimer()
+		r, err := http.NewRequest("GET", "/hello/blake", nil)
+		if err != nil {
+			panic(err)
+		}
+		b.StartTimer()
 		p.ServeHTTP(nil, r)
 	}
 }
