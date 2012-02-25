f328e02a8e3ffe2eeffbeef468349b3381dcabbd
George Rogers <georgerogers42@aol.com>
2012-02-25 02:14:36 -0600

    Improve API Docs

--- mux.go
+++ mux.go
@@ -1,3 +1,4 @@
+// pat.go a sinatra like muxer for go.
 package pat
 
 import (
@@ -9,10 +10,12 @@ type PatternServeMux struct {
 	handlers map[string][]*patHandler
 }
 
+// Creates an new *PatternServeMux.
 func New() *PatternServeMux {
 	return &PatternServeMux{make(map[string][]*patHandler)}
 }
 
+// Implements HttpHandler.
 func (p *PatternServeMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	for _, ph := range p.handlers[r.Method] {
 		if params, ok := ph.try(r.URL.Path); ok {
@@ -27,22 +30,27 @@ func (p *PatternServeMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	http.NotFound(w, r)
 }
 
+// Adds the pattern handler pair for a Get.
 func (p *PatternServeMux) Get(pat string, h http.Handler) {
 	p.Add("GET", pat, h)
 }
 
+// Adds the pattern handler pair for a Post.
 func (p *PatternServeMux) Post(pat string, h http.Handler) {
 	p.Add("POST", pat, h)
 }
 
+// Adds the pattern handler pair for a Put.
 func (p *PatternServeMux) Put(pat string, h http.Handler) {
 	p.Add("PUT", pat, h)
 }
 
+// Adds the pattern handler pair for a Delete.
 func (p *PatternServeMux) Del(pat string, h http.Handler) {
 	p.Add("DELETE", pat, h)
 }
 
+// Adds the pattern handler pair for a HTTP Method meth.
 func (p *PatternServeMux) Add(meth, pat string, h http.Handler) {
 	p.handlers[meth] = append(p.handlers[meth], &patHandler{pat, h})
 }
