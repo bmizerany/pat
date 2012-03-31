bd06fd7581d03fbdf7a9fb78486dd3dcf00f5da1
Blake Mizerany <blake.mizerany@gmail.com>
2012-03-31 15:21:36 -0700

    add comment to example

--- example/hello.go
+++ example/hello.go
@@ -16,6 +16,8 @@ func main() {
 	m := pat.New()
 	m.Get("/hello/:name", http.HandlerFunc(HelloServer))
 
+	// Register this pat with the default serve mux so that other packages
+	// may also be exported. (i.e. /debug/pprof/*)
 	http.Handle("/", m)
 	err := http.ListenAndServe(":12345", nil)
 	if err != nil {
