85c4867c533468474ef22ce4cbcdb65544e0eaab
Mike Stipicevic <stipredirect@gmail.com>
2012-05-12 20:16:25 -0700

    Added app engine example

---
+++ example/hello_appengine.go
@@ -0,0 +1,23 @@
+// hello.go ported for app engine
+
+package patexample
+
+import (
+    "io"
+    "net/http"
+    "pat"
+)
+
+// hello world, the web server
+func HelloServer(w http.ResponseWriter, req *http.Request) {
+    io.WriteString(w, "hello, "+req.URL.Query().Get(":name")+"!\n")
+}
+
+func init() {
+    m := pat.New()
+    m.Get("/hello/:name", http.HandlerFunc(HelloServer))
+
+    // Register this pat with the default serve mux so that other packages
+    // may also be exported. (i.e. /debug/pprof/*)
+    http.Handle("/", m)
+}
