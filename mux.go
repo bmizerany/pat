7ea176483e2eae248aca4fc31e9ca6b59b063667
Blake Mizerany <blake.mizerany@gmail.com>
2012-06-13 11:26:07 -0700

    add clarity to bounds check

--- mux.go
+++ mux.go
@@ -207,7 +207,7 @@ func (ph *patHandler) try(path string) (url.Values, bool) {
 	for i < len(path) {
 		switch {
 		case j >= len(ph.pat):
-			if ph.pat != "/" && len(ph.pat)-1 >= 0 && ph.pat[len(ph.pat)-1] == '/' {
+			if ph.pat != "/" && len(ph.pat) > 0 && ph.pat[len(ph.pat)-1] == '/' {
 				return p, true
 			}
 			return nil, false
