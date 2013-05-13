1173af554a34a2cfd52f8bb18aa22a2cc77bf9df
Sindre Føring Devik <sindre.devik@gmail.com>
2013-05-13 22:14:24 +0200

    When adding handler, pat did not check whether a handler already was registered. Now it updates the handler if it already exists

--- mux.go
+++ mux.go
@@ -168,7 +168,16 @@ func (p *PatternServeMux) Options(pat string, h http.Handler) {
 
 // Add will register a pattern with a handler for meth requests.
 func (p *PatternServeMux) Add(meth, pat string, h http.Handler) {
-	p.handlers[meth] = append(p.handlers[meth], &patHandler{pat, h})
+	n := &patHandler{pat, h})
+	if m, ok := p.handlers[meth]; ok {
+		for i, p := range m {
+			if p.pat == pat {
+				p.handlers[meth][i] = n
+				return
+			}
+		}
+	}
+	p.handlers[meth] = append(p.handlers[meth], n) 
 
 	n := len(pat)
 	if n > 0 && pat[n-1] == '/' {
