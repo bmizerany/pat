f5867be061937a836257d898c7c49bf075822980
Bryan Matsuo <bryan.matsuo@gmail.com>
2014-04-23 03:29:20 -0700

    fix a bug in Tail

--- mux.go
+++ mux.go
@@ -57,23 +57,23 @@ import (
 // A trivial example server is:
 //
 //	package main
-//	
+//
 //	import (
 //		"io"
 //		"net/http"
 //		"github.com/bmizerany/pat"
 //		"log"
 //	)
-//	
+//
 //	// hello world, the web server
 //	func HelloServer(w http.ResponseWriter, req *http.Request) {
 //		io.WriteString(w, "hello, "+req.URL.Query().Get(":name")+"!\n")
 //	}
-//	
+//
 //	func main() {
 //		m := pat.New()
 //		m.Get("/hello/:name", http.HandlerFunc(HelloServer))
-//	
+//
 //		// Register this pat with the default serve mux so that other packages
 //		// may also be exported. (i.e. /debug/pprof/*)
 //		http.Handle("/", m)
@@ -189,7 +189,7 @@ func Tail(pat, path string) string {
 		switch {
 		case j >= len(pat):
 			if pat[len(pat)-1] == '/' {
-				return path[j-1:]
+				return path[i:]
 			}
 			return ""
 		case pat[j] == ':':
